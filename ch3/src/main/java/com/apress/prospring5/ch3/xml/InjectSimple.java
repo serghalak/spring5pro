ba6f5d458a9abdfe76bbf20d22e0fff3b25b38dc
serghalak <serghalak@gmail.com>
2020-03-19 14:43:46 +0200

    use Spel with xml

---
+++ ch3/src/main/java/com/apress/prospring5/ch3/xml/InjectSimple.java
@@ -0,0 +1,81 @@
+package com.apress.prospring5.ch3.xml;
+
+import com.apress.prospring5.ch3.annotated.Singer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+@Service("injectSimple")
+public class InjectSimple {
+
+    @Value("John Mayer")
+    private String name;
+    @Value("40")
+    private int age;
+    @Value("1.92")
+    private float height;
+    @Value("false")
+    private boolean programmer;
+    @Value("1241401112")
+    private Long ageInSeconds;
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext context=
+                new GenericXmlApplicationContext();
+        context.load("classpath:spring/app-context-xml.xml");
+        context.refresh();
+        InjectSimple simple = context.getBean(InjectSimple.class);
+        context.close();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public boolean isProgrammer() {
+        return programmer;
+    }
+
+    public void setProgrammer(boolean programmer) {
+        this.programmer = programmer;
+    }
+
+    public Long getAgeInSeconds() {
+        return ageInSeconds;
+    }
+
+    public void setAgeInSeconds(Long ageInSeconds) {
+        this.ageInSeconds = ageInSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return "InjectSimple{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", programmer=" + programmer +
+                ", ageInSeconds=" + ageInSeconds +
+                '}';
+    }
+}
