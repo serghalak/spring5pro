67647a86005c1cbb33b397c23d99ebae03b1e0fc
serghalak <serghalak@gmail.com>
2020-03-20 16:20:25 +0200

    collection xml

---
+++ ch3/src/main/java/com/apress/prospring5/ch3/xml/LyricHolder.java
@@ -0,0 +1,10 @@
+package com.apress.prospring5.ch3.xml;
+
+public class LyricHolder implements ContentHolder{
+        private String value = "'You be the DJ, I'll be the driver'";
+        @Override public String toString() {
+
+            return "LyricHolder: { " + value + "}";
+        }
+
+}
