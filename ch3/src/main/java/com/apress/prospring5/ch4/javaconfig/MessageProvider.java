9341f227bb962a74d8a83a8ee685f9fdb6b6aece
serghalak <serghalak@gmail.com>
2020-04-09 12:06:02 +0300

    Revert "add Java config with properties file"
    This reverts commit bbc813b83c845320d7c7b251b3bcabffeac36482.

--- ch3/src/main/java/com/apress/prospring5/ch4/javaconfig/MessageProvider.java
+++
@@ -1,5 +0,0 @@
-package com.apress.prospring5.ch4.javaconfig;
-
-public interface MessageProvider {
-    String getMessage();
-}
--- ch3/src/main/java/com/apress/prospring5/ch4/javaconfig/MessageRenderer.java
+++
@@ -1,7 +0,0 @@
-package com.apress.prospring5.ch4.javaconfig;
-
-public interface MessageRenderer {
-    void render();
-    void setMessageProvider(MessageProvider provider);
-    MessageProvider getMessageProvider();
-}
