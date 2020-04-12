7696ad405685140c5585168f2fb57525f74b5b3f
serghalak <serghalak@gmail.com>
2020-04-12 19:46:28 +0300

    received data from properties file

---
+++ ch3/src/main/java/com/apress/prospring5/ch4/property/PlaceHolderDemo.java
@@ -0,0 +1,15 @@
+package com.apress.prospring5.ch4.property;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class PlaceHolderDemo {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx=new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/ch4/app-context-xml.xml");
+        ctx.refresh();
+        AppProperty appProperty=ctx.getBean("appProperty",AppProperty.class);
+        System.out.println("application.home: " + appProperty.getApplicationHome());
+        System.out.println("user.home: " + appProperty.getUserHome());
+        ctx.close();
+    }
+}
