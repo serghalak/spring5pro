522dbcd650f59a86dc6258f29f007c45315413c0
serghalak <serghalak@gmail.com>
2020-04-12 19:16:30 +0300

    Revert "add Java config with properties file"
    This reverts commit bbc813b83c845320d7c7b251b3bcabffeac36482.

---
+++ ch3/src/main/java/com/apress/prospring5/ch4/EnvironmentSampleLast.java
@@ -0,0 +1,31 @@
+package com.apress.prospring5.ch4;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnvironmentSampleLast {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx=new GenericXmlApplicationContext();
+        ctx.refresh();
+        ConfigurableEnvironment env = ctx.getEnvironment();
+        MutablePropertySources propertySources = env.getPropertySources();
+        Map<String,Object> appMap=new HashMap<>();
+        appMap.put("application.home","application_home");
+        propertySources.addLast(
+                new MapPropertySource("prospring5_MAP",appMap));
+
+        System.out.println("user.home: " + System.getProperty("user.home"));
+        System.out.println("JAVA_HOME: " + System.getenv("JAVA_HOME"));
+        System.out.println("user.home: " + env.getProperty("user.home"));
+        System.out.println("JAVA_HOME: " + env.getProperty("JAVA_HOME"));
+
+        ctx.close();
+        System.out.println("aplication.home: "
+                + env.getProperty("application.home"));
+    }
+}
